6e11af1d1baf40337b052100f7d1ec55d9130d33
gourav221b <57912157+gourav221b@users.noreply.github.com>
2021-06-01 21:12:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 const sounds = document.querySelectorAll('.sound');
 const pads = document.querySelectorAll('.pads div');
-const button = document.querySelector('.btn');
-var playlist = [];
 window.addEventListener('load', () => {
-
-
-    // MUSIC MAKING
-
     pads.forEach((pad, index) => {
         pad.addEventListener('click', function() {
             sounds[index].currentTime = 0;
             sounds[index].play();
-            console.log(sounds[index].duration);
-            // playlist.push(sounds[index].src);
-            console.log(playlist);
-
-        });
-
-
-
-
+       });
     });
-
-
-
 });
